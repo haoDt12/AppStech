cf3f9a68182ad81e510e878d5b6a5b3f3aeed63c
minhhtn <sachoi3.14@gmail.com>
2023-09-25 10:23:55 +0700

    giao dien logout

---
+++ app/src/main/java/com/datn/shopsale/ui/cart/CartFragment.java
@@ -0,0 +1,29 @@
+package com.datn.shopsale.ui.cart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.datn.shopsale.R;
+
+public class CartFragment extends Fragment {
+    public CartFragment() {
+    }
+    public static CartFragment newInstance() {
+        CartFragment fragment = new CartFragment();
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
--- app/src/main/java/com/datn/shopsale/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/datn/shopsale/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.datn.shopsale.ui.dashboard;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.datn.shopsale.R;
 import com.datn.shopsale.databinding.FragmentDashboardBinding;
+
+import java.util.Objects;
 
 public class DashboardFragment extends Fragment {
     public static DashboardFragment newInstance() {
@@ -37,21 +41,23 @@ public class DashboardFragment extends Fragment {
     }
     private Button btnLogOut;
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        btnLogOut = (Button) view.findViewById(R.id.btn_log_out);
+        btnLogOut = view.findViewById(R.id.btn_log_out);
         btnLogOut.setOnClickListener(view1 -> {
             Dialog dialog = new Dialog(view1.getContext());
             dialog.setContentView(R.layout.dialog_log_out);
-//            dialog.getWindow().setBackgroundDrawable(view1.getContext().getDrawable(R.drawable.));
+            Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(view1.getContext().getDrawable(R.drawable.dialog_bg));
+            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
             Window window = dialog.getWindow();
             window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
             WindowManager.LayoutParams windowAttributes = window.getAttributes();
             window.setAttributes(windowAttributes);
             windowAttributes.gravity = Gravity.BOTTOM;
 
-            Button btnCancel = (Button) dialog.findViewById(R.id.btn_cancel);
-            Button btnConfirm = (Button) dialog.findViewById(R.id.btn_confirm);
+            ImageButton btnCancel = dialog.findViewById(R.id.btn_cancel);
+            Button btnConfirm = dialog.findViewById(R.id.btn_confirm);
             btnCancel.setOnClickListener(view2 -> {
                 dialog.cancel();
             });
