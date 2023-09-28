10559c23380315df401817aecea6f1335d45943d
dinhtronghao <dinhtronghao12102002@gmail.com>
2023-09-28 21:59:25 +0700

    layout list,itemUser

---
+++ app/src/main/java/com/datn/shopsale/ui/listUser/ListUserFragment.java
@@ -0,0 +1,40 @@
+package com.datn.shopsale.ui.listUser;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.datn.shopsale.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ListUserFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ListUserFragment extends Fragment {
+
+    public ListUserFragment() {
+        // Required empty public constructor
+    }
+
+    public static ListUserFragment newInstance() {
+        ListUserFragment fragment = new ListUserFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list_user, container, false);
+    }
+}
