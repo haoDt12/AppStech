394c8c2f43e5eb2ec4afc6fbd89cfa1cc14c0b3e
tungktph27526 <tungktph27526@fpt.edu.vn>
2023-10-02 21:46:02 +0700

    Layout_store

--- app/src/main/java/com/datn/shopsale/ui/store/StoreActivity.java
+++ app/src/main/java/com/datn/shopsale/ui/store/StoreActivity.java
@@ -11,6 +11,8 @@ public class StoreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_store);
     }
 }
