d6711d21d29f1294658bb420239ccf6d65f930b5
tungdeptrai <tungktph27675@fpt.edu.vn>
2023-10-04 09:42:50 +0700

    Code giao diện MÀn hình Thêm Địa chỉ

---
+++ app/src/main/java/com/datn/shopsale/ui/dashboard/address/AddAddressActivity.java
@@ -0,0 +1,16 @@
+package com.datn.shopsale.ui.dashboard.address;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.datn.shopsale.R;
+
+public class AddAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_address);
+    }
+}
