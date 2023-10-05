d5a286c639a65eaa565291dd9af7a33233f7e119
dinhtronghao <dinhtronghao12102002@gmail.com>
2023-10-05 23:08:27 +0700

    layout review

--- app/src/main/java/com/datn/shopsale/activities/ReviewActivity.java
+++ app/src/main/java/com/datn/shopsale/activities/ReviewActivity.java
@@ -12,5 +12,6 @@ public class ReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
     }
 }
