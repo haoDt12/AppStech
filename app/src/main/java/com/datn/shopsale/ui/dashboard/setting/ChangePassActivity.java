fcdc64dfc805f43c3533c7e611ea84c250e893b2
trinhdien <trinhdien606@gmail.com>
2023-12-12 11:44:35 +0700

    update edit pass and reset pass

--- app/src/main/java/com/datn/shopsale/ui/dashboard/setting/ChangePassActivity.java
+++ app/src/main/java/com/datn/shopsale/ui/dashboard/setting/ChangePassActivity.java
@@ -1,9 +1,6 @@
 package com.datn.shopsale.ui.dashboard.setting;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,12 +8,23 @@ import android.view.Gravity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.datn.shopsale.Interface.ApiService;
 import com.datn.shopsale.R;
+import com.datn.shopsale.request.EditPassRequest;
+import com.datn.shopsale.response.EditPasswordResponse;
+import com.datn.shopsale.response.VerifyOtpEditPassResponse;
+import com.datn.shopsale.retrofit.RetrofitConnection;
+import com.datn.shopsale.utils.AlertDialogUtil;
+import com.datn.shopsale.utils.LoadingDialog;
+import com.datn.shopsale.utils.PreferenceManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -25,14 +33,20 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ChangePassActivity extends AppCompatActivity {
     private TextInputEditText edPass,edPassNew,edRepassNew;
     private TextView tvError;
     private Toolbar toolbarChangePass;
     private Button btnSend;
-
-
-
+    private ApiService apiService;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,44 +103,128 @@ public class ChangePassActivity extends AppCompatActivity {
         edPassNew = (TextInputEditText) findViewById(R.id.ed_passNew);
         edRepassNew = (TextInputEditText) findViewById(R.id.ed_RepassNew);
         btnSend = (Button) findViewById(R.id.btn_send);
-        btnSend.setOnClickListener(v->{
-//            if(isFieldEmpty(edPass) || isFieldEmpty(edPassNew) || isFieldEmpty(edRepassNew)){
-//                Toast.makeText(this, "Không được để trống", Toast.LENGTH_SHORT).show();
-//            }else{
-//                changePassword();
-//            }
-            Dialog dialog = new Dialog(v.getContext());
-            dialog.setContentView(R.layout.dialog_otp_change_password);
-            Window window = dialog.getWindow();
-            window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-            window.setBackgroundDrawable(v.getContext().getDrawable(R.drawable.dialog_bg));
-            window.getAttributes().windowAnimations = R.style.DialogAnimation;
-            WindowManager.LayoutParams windowAttributes = window.getAttributes();
-            window.setAttributes(windowAttributes);
-            windowAttributes.gravity = Gravity.BOTTOM;
-            TextInputEditText edOtp = dialog.findViewById(R.id.ed_otp);
-            Button btnSendOtp = dialog.findViewById(R.id.btn_sendOtp);
-            ImageButton btnCancel = dialog.findViewById(R.id.btn_cancel);
-            btnCancel.setOnClickListener(view -> {
-                dialog.dismiss();
-            });
-            btnSendOtp.setOnClickListener(view -> {
-//                dialog.show();
-//                btnSendOtp.setEnabled(false);
-                dialog.dismiss();
-            });
-            dialog.show();
-        });
+        apiService = RetrofitConnection.getApiService();
+        preferenceManager = new PreferenceManager(this);
         setSupportActionBar(toolbarChangePass);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.angle_left);
         toolbarChangePass.setNavigationOnClickListener(v -> {
             onBackPressed();
         });
+        onClickSend();
     }
     // check rỗng
     private boolean isFieldEmpty(TextInputEditText editText) {
         String text = editText.getText().toString().trim();
         return TextUtils.isEmpty(text);
+    }
+    private static boolean isValidPassword(String password) {
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        Pattern pattern = Pattern.compile(passwordRegex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+    private void editPassword(String currentPass,String newPass){
+        LoadingDialog.showProgressDialog(this,"Loading...");
+        EditPassRequest request = new EditPassRequest();
+        request.setUserId(preferenceManager.getString("userId"));
+        request.setCurrentPass(currentPass);
+        request.setNewPass(newPass);
+        Call<EditPasswordResponse> call = apiService.editPassword(preferenceManager.getString("token"),request);
+        call.enqueue(new Callback<EditPasswordResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<EditPasswordResponse> call, @NonNull Response<EditPasswordResponse> response) {
+                runOnUiThread(() -> {
+                    assert response.body() != null;
+                    LoadingDialog.dismissProgressDialog();
+                    if(response.body().getCode() == 1){
+                        Dialog dialog = new Dialog(ChangePassActivity.this);
+                        dialog.setContentView(R.layout.dialog_otp_change_password);
+                        Window window = dialog.getWindow();
+                        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                        window.setBackgroundDrawable(ChangePassActivity.this.getDrawable(R.drawable.dialog_bg));
+                        window.getAttributes().windowAnimations = R.style.DialogAnimation;
+                        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+                        window.setAttributes(windowAttributes);
+                        windowAttributes.gravity = Gravity.BOTTOM;
+                        TextInputEditText edOtp = dialog.findViewById(R.id.ed_otp);
+                        Button btnSendOtp = dialog.findViewById(R.id.btn_sendOtp);
+                        ImageButton btnCancel = dialog.findViewById(R.id.btn_cancel);
+                        btnCancel.setOnClickListener(view -> {
+                            dialog.dismiss();
+                        });
+                        btnSendOtp.setOnClickListener(view -> {
+                            if(isFieldEmpty(edOtp)){
+                                Toast.makeText(ChangePassActivity.this, "otp không được để trống", Toast.LENGTH_SHORT).show();
+                            }
+                            senOtpPassword(edOtp.getText().toString());
+                        });
+                        dialog.show();
+                    }else {
+                        AlertDialogUtil.showSimpleAlertDialog(ChangePassActivity.this,response.body().getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<EditPasswordResponse> call, @NonNull Throwable t) {
+                runOnUiThread(() -> {
+                    LoadingDialog.dismissProgressDialog();
+                    AlertDialogUtil.showAlertDialogWithOk(ChangePassActivity.this,t.getMessage());
+                });
+            }
+        });
+    }
+    private void senOtpPassword(String otp){
+        LoadingDialog.showProgressDialog(this,"Loading...");
+        EditPassRequest request = new EditPassRequest();
+        request.setUserId(preferenceManager.getString("userId"));
+        request.setOtp(otp);
+        Call<VerifyOtpEditPassResponse> call = apiService.sendOtpPassword(preferenceManager.getString("token"),request);
+        call.enqueue(new Callback<VerifyOtpEditPassResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<VerifyOtpEditPassResponse> call, @NonNull Response<VerifyOtpEditPassResponse> response) {
+                runOnUiThread(() -> {
+                    LoadingDialog.dismissProgressDialog();
+                    assert response.body() != null;
+                    if(response.body().getCode() == 1){
+                        new AlertDialog.Builder(ChangePassActivity.this)
+                                .setTitle("Notification")
+                                .setMessage(response.body().getMessage())
+                                .setPositiveButton("OK", (dialog1, which) -> finish())
+                                .show();
+                    }else {
+                        AlertDialogUtil.showSimpleAlertDialog(ChangePassActivity.this,response.body().getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<VerifyOtpEditPassResponse> call, @NonNull Throwable t) {
+                runOnUiThread(() -> {
+                    AlertDialogUtil.showAlertDialogWithOk(ChangePassActivity.this,t.getMessage());
+                });
+            }
+        });
+    }
+    private void onClickSend(){
+        btnSend.setOnClickListener(v->{
+            if(isFieldEmpty(edPass) || isFieldEmpty(edPassNew) || isFieldEmpty(edRepassNew)){
+                Toast.makeText(this, "Không được để trống", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(!isValidPassword(edPassNew.getText().toString())){
+                Toast.makeText(this, "Mật khẩu tối thiểu 8 ký tự, ít nhất 1 chữ in hoa, 1 số và 1 ký tự đặc biệt", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(!edPassNew.getText().toString().equals(edRepassNew.getText().toString())){
+                Toast.makeText(this, "Mật khẩu mới không khớp", Toast.LENGTH_SHORT).show();
+                return;
+            }
+//            else{
+//                changePassword();
+//            }
+            editPassword(edPass.getText().toString(),edPassNew.getText().toString());
+        });
     }
 }
--- app/src/main/java/com/datn/shopsale/ui/login/ForgotPassActivity.java
+++ app/src/main/java/com/datn/shopsale/ui/login/ForgotPassActivity.java
@@ -1,9 +1,5 @@
 package com.datn.shopsale.ui.login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,28 +10,48 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.datn.shopsale.Interface.ApiService;
 import com.datn.shopsale.R;
+import com.datn.shopsale.response.GetPassResponse;
+import com.datn.shopsale.retrofit.RetrofitConnection;
+import com.datn.shopsale.utils.AlertDialogUtil;
+import com.datn.shopsale.utils.LoadingDialog;
+import com.datn.shopsale.utils.PreferenceManager;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ForgotPassActivity extends AppCompatActivity {
     private TextInputEditText edEmail;
     private Button btnSend;
     private Toolbar toolbarForgotPass;
     private ProgressBar progressBar;
+    private PreferenceManager preferenceManager;
+    private ApiService apiService;
     private static final int PROGRESS_DELAY = 5000; // Thời gian đặt trước 5 giây
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
         init();
     }
+
     public void resetUserPassword(String email) {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         progressBar.setVisibility(View.VISIBLE); // Hiển thị ProgressBar khi bắt đầu
@@ -67,46 +83,105 @@ public class ForgotPassActivity extends AppCompatActivity {
                     }
                 });
     }
-    private void init(){
+
+    private void init() {
         toolbarForgotPass = (Toolbar) findViewById(R.id.toolbar_forgot_pass);
         edEmail = (TextInputEditText) findViewById(R.id.ed_email);
         btnSend = (Button) findViewById(R.id.btn_send);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        btnSend.setOnClickListener(v->{
+        apiService = RetrofitConnection.getApiService();
+        preferenceManager = new PreferenceManager(this);
+        btnSend.setOnClickListener(v -> {
             String email = edEmail.getText().toString().trim();
-//            if(email.isEmpty()){
-//                Toast.makeText(this, "Vui lòng nhập email", Toast.LENGTH_SHORT).show();
-//            }else{
-//                resetUserPassword(email);
-//            }
-            Dialog dialog = new Dialog(v.getContext());
-            dialog.setContentView(R.layout.dialog_check_email);
-            Window window = dialog.getWindow();
-            window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-            window.setBackgroundDrawable(v.getContext().getDrawable(R.drawable.dialog_bg));
-            window.getAttributes().windowAnimations = R.style.DialogAnimation;
-            WindowManager.LayoutParams windowAttributes = window.getAttributes();
-            window.setAttributes(windowAttributes);
-            windowAttributes.gravity = Gravity.BOTTOM;
-            TextView tvContent = dialog.findViewById(R.id.tv_content);
-            Button btnConfirm = dialog.findViewById(R.id.btn_confirm);
-            ImageButton btnCancel = dialog.findViewById(R.id.btn_cancel);
-            btnCancel.setOnClickListener(view -> {
-                dialog.dismiss();
-            });
-            btnConfirm.setOnClickListener(view -> {
-//                dialog.show();
-//                btnSendOtp.setEnabled(false);
-                dialog.dismiss();
-            });
-            dialog.show();
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Vui lòng nhập email", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!isValidEmail(email) && !isNumeric(email)) {
+                Toast.makeText(this, "Email không đúng định dạng", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (!isValidPhoneNumber(email) && isNumeric(email)) {
+                Toast.makeText(this, "Số điện thoại không đúng định dạng", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            onResetPass(email);
         });
         setSupportActionBar(toolbarForgotPass);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.angle_left);
         toolbarForgotPass.setNavigationOnClickListener(v -> {
             onBackPressed();
+        });
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidPhoneNumber(String phoneNumber) {
+        String phoneNumberRegex = "^(?:\\+84|0)[1-9]\\d{8}$";
+        Pattern pattern = Pattern.compile(phoneNumberRegex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    private static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private void onResetPass(String username) {
+        LoadingDialog.showProgressDialog(ForgotPassActivity.this, "Loading...");
+        Call<GetPassResponse> call = apiService.getPassWord(preferenceManager.getString("token"), username);
+        call.enqueue(new Callback<GetPassResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<GetPassResponse> call, @NonNull Response<GetPassResponse> response) {
+                runOnUiThread(() -> {
+                    LoadingDialog.dismissProgressDialog();
+                    if (response.body().getCode() == 1) {
+                        Dialog dialog = new Dialog(ForgotPassActivity.this);
+                        dialog.setContentView(R.layout.dialog_check_email);
+                        Window window = dialog.getWindow();
+                        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                        window.setBackgroundDrawable(ForgotPassActivity.this.getDrawable(R.drawable.dialog_bg));
+                        window.getAttributes().windowAnimations = R.style.DialogAnimation;
+                        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+                        window.setAttributes(windowAttributes);
+                        windowAttributes.gravity = Gravity.BOTTOM;
+                        Button btnConfirm = dialog.findViewById(R.id.btn_confirm);
+                        ImageButton btnCancel = dialog.findViewById(R.id.btn_cancel);
+                        btnCancel.setOnClickListener(view -> {
+                            dialog.dismiss();
+                        });
+                        btnConfirm.setOnClickListener(view -> {
+                            dialog.dismiss();
+                        });
+                        dialog.show();
+                    } else {
+                        LoadingDialog.dismissProgressDialog();
+                        AlertDialogUtil.showAlertDialogWithOk(ForgotPassActivity.this, response.body().getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<GetPassResponse> call, @NonNull Throwable t) {
+                runOnUiThread(() -> {
+                    LoadingDialog.dismissProgressDialog();
+                    AlertDialogUtil.showAlertDialogWithOk(ForgotPassActivity.this, t.getMessage());
+                });
+            }
         });
     }
 }
